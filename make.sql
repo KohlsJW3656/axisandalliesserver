3368cbab2ce86ecce1e9f1b7130dc567ecef7ac7
kohlsjw3656 <kohlsjw3656@gmail.com>
2020-10-01 15:58:04 -0500

    Ignore file

--- make.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS axisandallies;
-DROP USER IF EXISTS aaUser@localhost;
-
-CREATE DATABASE axisandallies CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER aaUser@localhost IDENTIFIED BY 'wQynVfAg1$';
-GRANT ALL PRIVILEGES ON axisandallies.* TO aaUser@localhost;
